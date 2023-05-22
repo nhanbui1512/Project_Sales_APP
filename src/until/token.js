36555f36b37e932355db0c5e274210e0f0263358
nhanbui1512 <buithiennhan0345@gmail.com>
2023-05-22 23:04:54 +0700

    update multer for API user

--- src/until/token.js
+++ src/until/token.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports =
-{
-   GenerateAccpectToken(user)
-   {
-      return jwt.sign( {
-                  IDUser: user.IDUser,
-                  access: user.Access,
-                  userName: user.UserName,
-               },process.env.JWT_PASS,
-               {
-                  expiresIn:"30d"
-               });
-   }
-}   
+module.exports = {
+    GenerateAccpectToken(user) {
+        return jwt.sign(
+            {
+                IDUser: user.IDUser,
+                access: user.Access,
+                userName: user.UserName,
+            },
+            process.env.JWT_PASS,
+            {
+                expiresIn: '30d',
+            },
+        );
+    },
+};
